7466b5c32ea2fcb472edea17fb697dba14f675c0
Zaki <gostudyzaki@gmail.com>
2025-03-01 22:36:55 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'PassManager';
+
+const app = express()
+const port = 3000
+
+client.connect();
+
+app.get('/', async(req, res) => {
+    const db = client.db(dbName);
+  const collection = db.collection('documents');    
+  const findResult = await collection.find({}).toArray();
+  res.json(findResult)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
